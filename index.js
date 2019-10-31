7d8ef963223869969c253be99638426205764135
frankie <877672473@qq.com>
2019-10-31 15:54:07 +0800

    mutation

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
-const { graphql, buildSchema } = require('graphql')
+const app = new (require('koa'))
+const mount = require('koa-mount')
+const static = require('koa-static')
+const graphqlHTTP = require('koa-graphql')
 
-const schema = buildSchema(`
-  type Query {
-    hello: String
-  }
-`)
-
-const root = {
-  hello: () => 'hello'
-}
-
-
-module.exports = function(query) {
-  return graphql(schema, query, root).then((response) => {
-    return response
+app.use(
+  graphqlHTTP({
+    schema: require('./schema')
   })
-}
+)
+
+app.listen(3000)
---
+++ schema.js
@@ -0,0 +1,57 @@
+const { buildSchema } = require('graphql')
+
+const mockDatabase = {
+  1: {
+    id: 1,
+    avatar: 'String',
+    name: 'String',
+    isTop: true,
+    content: 'String',
+    publishDate: 'String',
+    commentNum: 3,
+    praiseNum: 4
+  },
+  2: {
+    id: 2,
+    avatar: 'String',
+    name: 'String',
+    isTop: true,
+    content: 'String',
+    publishDate: 'String',
+    commentNum: 3,
+    praiseNum: 4
+  }
+}
+
+const schema = buildSchema(`
+  type Comment {
+    id: Int
+    avatar: String
+    name: String
+    isTop: Boolean
+    content: String
+    publishDate: String
+    commentNum: Int
+    praiseNum: Int
+  }
+
+  type Query {
+    comment: [Comment]
+  }
+
+  type Mutation {
+    praise(id: Int): Int
+  }
+
+`)
+
+schema.getQueryType().getFields().comment.resolve = () => {
+  return Object.keys(mockDatabase).map(key => mockDatabase[key])
+}
+
+schema.getMutationType.getFields().praise.resolve = (id) => {
+  mockDatabase[id].praiseNum++
+  return mockDatabase[id].praiseNum
+}
+
+module.exports = schema
